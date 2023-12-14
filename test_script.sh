8c568a9fcd500e36d97cbf29d64073566eebe281
MerimKh01 <MrmKh2001@gmail.com>
2023-12-14 02:45:05 +0100

    Shell project 1

---
+++ test_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "/bin/ls" | ./hsh
+/bin/ls
+/bin/ls
+/bin/ls
+/bin/ls
+/bin/ls -l
+cp /bin/ls hbtn_ls
+./hbtn_ls /var
+echo "    " | ./hsh
+echo "          " | ./hsh
+echo "     " | ./hsh
